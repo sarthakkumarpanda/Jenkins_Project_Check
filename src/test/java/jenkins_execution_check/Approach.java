033ecbabb4fb6dc171bb96fda6124d6a2fa9895e
sarthakkumarpanda <sarthak.matrix@gmail.com>
2024-02-28 19:41:29 -0500

    Checking for jenkins

---
+++ src/test/java/jenkins_execution_check/Approach.java
@@ -0,0 +1,15 @@
+package jenkins_execution_check;
+
+public class Approach {
+	
+	
+	//We identify our Test Scenarios - Login, Register, SearchProduct
+	//Each Scenario pertains to one Functionality what we are testing
+	//We write Test Cases in different Classes - positive and negative
+	//Run all the Test Cases together with testng.xml file
+	//Generally there is a lot of code which is quite common in all the Test Cases
+	//We move the common code which is used again and again to one particular Class
+	//This Class acts as the Parent Class
+	//Other Classes act as the Child Classes
+
+}
